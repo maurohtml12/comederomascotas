d6eeadd853b3ddfbba4f12965e88ad45e01ae982
maurohtml12 <mauricio.sobarzo.h@gmail.com>
2021-11-19 15:49:46 -0300

    pre-alpha release comedero mascotas
    //add activity main
    //add set clock for food

---
+++ app/src/main/java/com/example/mqttprueba/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mqttprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tempo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tempo = findViewById(R.id.txttime);
+    }
+    public void horacomida(View View)
+    {
+        Calendar cal = Calendar.getInstance();
+        int hora = cal.get(Calendar.HOUR_OF_DAY);
+        int minutos = cal.get(Calendar.MINUTE);
+
+        TimePickerDialog tpd = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+
+                String hora =  hour + ":" + minute;
+                tempo.setText("horario de comida Programado : " + hora+ "horas");
+            }
+        },hora, minutos, true);
+        tpd.show();
+
+    }
+}
